09f6bd2da61737a542b266e20d068490a62531df
Shyamanand Yadav <shyamayadav154@gmail.com>
2023-09-16 22:22:42 +0530

    feat: added mapping for harpoon and nvterm

--- mappings.lua
+++ mappings.lua
@@ -11,14 +11,35 @@ M.general = {
                 nowait = true
             }
         },
-        ["<leader>u"] = { ":UndotreeToggle<CR>" },
-        ["<leader>gs"] = { vim.cmd.Git },
+        ["<leader>u"] = { ":UndotreeToggle<CR>", 'Undo tree toggle' },
+        ["<leader>gs"] = { vim.cmd.Git, "Git commit status" },
+        ["<A-t>"] = { function ()
+            require("nvterm.terminal").toggle("horizontal")
+        end, "Terminal toggle horizontal" },
+        ["<leader>a"] = { function() require("harpoon.mark").add_file() end },
+        ["<leader>q"] = { function() require("harpoon.ui").toggle_quick_menu() end },
+        ["<leader>1"] = { function() require("harpoon.ui").nav_file(1) end },
+        ["<leader>2"] = { function() require("harpoon.ui").nav_file(2) end },
+        ["<leader>3"] = { function() require("harpoon.ui").nav_file(3) end },
+        ["<leader>4"] = { function() require("harpoon.ui").nav_file(4) end },
+
+    },
+    t = {
+        [";"] = {
+            ":",
+            opts = {
+                nowait = true
+            }
+        },
+        ["<A-t>"] = { function ()
+            require("nvterm.terminal").toggle("horizontal")
+        end }
     }
 }
 -- M.harpoon = {
 --     n = {
---         ["<leader>a"] = { require("harpoon.mark").add_file },
---         ["<leader>q"] = { require("harpoon.ui").toggle_quick_menu },
+--         ["<leader>a"] = { require("harpoon.mark").add_file() },
+--         ["<leader>q"] = { require("harpoon.ui").toggle_quick_menu() },
 --         ["<leader>1"] = { function() require("harpoon.ui").nav_file(1) end },
 --         ["<leader>2"] = { function() require("harpoon.ui").nav_file(2) end },
 --         ["<leader>3"] = { function() require("harpoon.ui").nav_file(3) end },
